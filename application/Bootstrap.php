07be9fbddabe0168aafda731be12dfe11c050337
Victor <stalin1945@gmail.com>
2011-02-08 00:42:48 +0200

    Added: Namespace and module bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,16 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+	protected function _initModule() {
+		$loader = new Zend_Application_Module_Autoloader(array(
+			'namespace' => '',
+			'basePath' => APPLICATION_PATH,
+		));
+		return $loader;
+	}
+	
+	protected function _initAutoloadNamespace() {
+		$this->getApplication()->getAutoloader()->registerNamespace(array('Skaya_'));
+	}
 
 }
-
