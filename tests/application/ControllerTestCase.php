0716705747d9578e5591f4572d72c4f89b7e1649
Victor Gryshko <stalin1945@gmail.com>
2011-02-11 18:52:25 +0200

    Added: Unit tests

---
+++ tests/application/ControllerTestCase.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'Zend/Application.php';
+require_once 'Zend/Test/PHPUnit/ControllerTestCase.php';
+
+abstract class ControllerTestCase extends Zend_Test_PHPUnit_ControllerTestCase
+{
+    /**
+     * @var Zend_Application
+     */
+    protected $_application;
+
+    public function setUp()
+    {
+        // указываем функцию, которая будет выполнена до запуска тестов
+        $this->bootstrap = array($this, 'appBootstrap');
+        parent::setUp();
+    }
+
+    public function appBootstrap()
+    {
+        // инициализируем наше приложение
+        $this->_application = new Zend_Application(
+            APPLICATION_ENV,
+            APPLICATION_PATH . '/configs/application.ini'
+        );
+
+        $this->_application->bootstrap();
+    }
+}
---
+++ tests/application/models/UserTest.php
@@ -0,0 +1,144 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UserTest
+ *
+ * @author victor
+ */
+
+namespace Model;
+
+class UserTest extends \ControllerTestCase {
+	
+	protected $_email = 'email@email.com';
+	
+	protected $_password = '123';
+	
+	protected $_user = null;
+	
+	public function setUp() {
+		parent::setUp();
+		$this->_resetUserModel();
+	}
+
+	public function testCanCreateUserModel() {
+		$user = new \Model\User();
+		$this->assertType('\\Model\\User', $user);
+	}
+	
+	public function testCanPopulate() {
+		$newEmail = 'newemail@email.com';
+		$newPassword = 'test123';
+		
+		//Populate from array
+		$this->_user->populate(array(
+			'email' => $newEmail,
+			'password' => $newPassword
+		));
+		$this->assertEquals($newEmail, $this->_user->email);
+		$this->assertEquals($newPassword, $this->_user->password);
+		
+		$this->_resetUserModel();
+		
+		//Populate from object
+		$newUserDefinition = new \stdClass();
+		$newUserDefinition->email = $newEmail;
+		$newUserDefinition->password = $newPassword;
+		$this->_user->populate($newUserDefinition);
+		$this->assertEquals($newEmail, $this->_user->email);
+		$this->assertEquals($newPassword, $this->_user->password);
+		
+		$this->_resetUserModel();
+		
+		//Popualte with Model
+		$anotherUser = new \Model\User();
+		$anotherUser->populate($this->_user);
+		$this->assertEquals($this->_email, $anotherUser->email);
+		$this->assertEquals($this->_password, $anotherUser->password);
+		
+		$this->_resetUserModel();
+	}
+	
+	public function testCanSetGetValues() {
+		$testValue = 'testValue';
+		$this->_user->someTestValue = $testValue;
+		$this->assertTrue(isset ($this->_user->someTestValue));
+		$this->assertEquals($testValue, $this->_user->someTestValue);
+	}
+	
+	public function testCanGetSetRole() {
+		//Getting default role without getter
+		$this->assertEquals(\Model\User::USER_ROLE_GUEST, $this->_user->role);
+
+		//Setting new Role and getting it with getter
+		$this->_user->role = \Model\User::USER_ROLE_ADMIN;
+		$this->assertEquals(\Model\User::USER_ROLE_ADMIN, $this->_user->getRole());
+	}
+
+	public function testIssetField() {
+		$this->assertTrue(isset($this->_user->email));
+		$this->assertFalse(isset($this->_user->junkField));
+	}
+	
+	public function testUnsetField() {
+		unset($this->_user->email);
+		$this->assertFalse(isset ($this->_user->email));
+	}
+	
+	public function testToArray() {
+		//Simple test
+		$array = $this->_user->toArray();
+		$this->assertType('array', $array);
+		$this->assertArrayHasKey('email', $array);
+		$this->assertArrayHasKey('password', $array);
+		$this->assertEquals($this->_email, $array['email']);
+		$this->assertEquals($this->_password, $array['password']);
+		
+		//Test with nested objects
+		$testValue = 'testValue';
+		
+		$this->_user->nestedSimpleObject = new \stdClass();
+		$this->_user->nestedSimpleObject->testValue = $testValue;
+		$this->_user->nestedModel = new \Model\User(array(
+			'testValue' => $testValue
+		));
+		$array = $this->_user->toArray();
+		$this->assertArrayHasKey('nestedSimpleObject', $array);
+		$this->assertArrayHasKey('nestedModel', $array);
+		$this->assertEquals($testValue, $array['nestedSimpleObject']['testValue']);
+		$this->assertEquals($testValue, $array['nestedModel']['testValue']);
+	}
+	
+	public function testGetSetMapperType() {
+		$testMapperType = 'db';
+		//Resetting current user mapper type
+		$this->_user->setMapperType(null);
+
+		//Test static default types
+		\Model\User::setDefaultMapperType($testMapperType);
+		$this->assertEquals($testMapperType, \Model\User::getDefaultMapperType());
+
+		//Testing getter without setting
+		$this->assertEquals(\Model\User::getDefaultMapperType(), $this->_user->getMapperType());
+
+		//Testing current model mapper types getter/setter
+		$this->_user->setMapperType($testMapperType);
+		$this->assertEquals($testMapperType, $this->_user->getMapperType());
+	}
+
+	protected function _resetUserModel() {
+		if (!$this->_user) {
+			$this->_user = new \Model\User();
+		}
+		$this->_user->populate(array(
+			'email' => $this->_email,
+			'password' => $this->_password
+		));
+	}
+	
+}
+?>
