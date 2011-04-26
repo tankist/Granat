c8f85053a0b5640c90f162e97d160663ff95aeef
Victor <stalin1945@gmail.com>
2011-04-27 00:53:28 +0300

    Added: ACL rules to the application config Added: navigation rules to the application config Added: session config to the application config Added: Navigation-driven helper for main menu in the layout

---
+++ application/modules/admin/Bootstrap.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: victor
+ * Date: 25.02.11
+ * Time: 16:20
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+class Admin_Bootstrap extends Zend_Application_Module_Bootstrap {
+
+	protected function _initSessionNamespace() {
+		$this->getApplication()->bootstrap('cache')->bootstrap('db')->bootstrap('session');
+
+		$session = new Zend_Session_Namespace($this->_moduleName);
+
+		return $session;
+	}
+
+	protected function _initAuthAcl() {
+		$this->bootstrap('acl');
+
+		$moduleName = strtolower($this->_moduleName);
+
+		$front = Zend_Controller_Front::getInstance();
+		$authPlugin = $front->getPlugin('Skaya_Controller_Plugin_Auth');
+		if (!$authPlugin) {
+			$authPlugin = new Skaya_Controller_Plugin_Auth(
+				Zend_Auth::getInstance(),
+				$this->getResource('acl'),
+				$moduleName
+			);
+			$front->registerPlugin($authPlugin);
+		}
+		else {
+			$authPlugin
+				->setAcl($this->getResource('acl'), $moduleName)
+				->setAuth(Zend_Auth::getInstance(), $moduleName);
+		}
+		$authPlugin->setIsSeparateAuthNamespace(true);
+		$options = $this->getOption('authacl');
+		foreach ((array)$options as $key => $value) {
+			if (strtolower($key) == 'noauth') {
+				$authPlugin->setNoAuthRules($value, $moduleName);
+			}
+			if (strtolower($key) == 'noacl') {
+				$authPlugin->setNoAclRules($value, $moduleName);
+			}
+		}
+		return $authPlugin;
+	}
+
+	protected function _initErrorHandler() {
+		$this->bootstrap('frontcontroller');
+		$front = Zend_Controller_Front::getInstance();
+		$errorOptions = array(
+			'module' => strtolower($this->_moduleName),
+			'controller' => 'error',
+			'action' => 'error'
+		);
+		$errorPlugin = new Skaya_Controller_Plugin_ErrorHandler($errorOptions);
+		$front->registerPlugin($errorPlugin, 101);
+	}
+
+	protected function _initPaginator() {
+		Zend_View_Helper_PaginationControl::setDefaultViewPartial('paginator.phtml');
+	}
+
+}
