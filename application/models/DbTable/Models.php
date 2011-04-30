c2dec556a0ee003f5faf60f3b8ff4cef693441a8
Victor <stalin1945@gmail.com>
2011-04-30 20:51:37 +0300

    Added: DBTable classes for model_categories link table

--- application/models/DbTable/Models.php
+++ application/models/DbTable/Models.php
@@ -6,7 +6,7 @@ class Model_DbTable_Models extends Skaya_Model_DbTable_Abstract {
 
 	protected $_primary = 'id';
 
-	protected $_dependentTables = array('Model_DbTable_Photos');
+	protected $_dependentTables = array('Model_DbTable_Photos', 'Model_DbTable_ModelCategories');
 
 	protected $_referenceMap = array('Collections' => array(
 		'columns' => array('collection_id'),
