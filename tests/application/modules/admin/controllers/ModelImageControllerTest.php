4280ea3673736e517b57c38c2dbb47e7a650928c
Victor Gryshko <stalin1945@gmail.com>
2011-05-06 18:26:31 +0300

    Added: New upload method view helpers Added: ModelIMageController tests

---
+++ tests/application/modules/admin/controllers/ModelImageControllerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+class Admin_ModelImageControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
+{
+
+    public function setUp()
+    {
+        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        parent::setUp();
+    }
+
+    public function testIndexAction()
+    {
+        $params = array('action' => 'index', 'controller' => 'ModelImage', 'module' => 'admin');
+        $url = $this->url($this->urlizeOptions($params));
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($params['module']);
+        $this->assertController($params['controller']);
+        $this->assertAction($params['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
+    public function testUploadAction()
+    {
+        $params = array('action' => 'upload', 'controller' => 'ModelImage', 'module' => 'admin');
+        $url = $this->url($this->urlizeOptions($params));
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($params['module']);
+        $this->assertController($params['controller']);
+        $this->assertAction($params['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
+    public function testDeleteAction()
+    {
+        $params = array('action' => 'delete', 'controller' => 'ModelImage', 'module' => 'admin');
+        $url = $this->url($this->urlizeOptions($params));
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($params['module']);
+        $this->assertController($params['controller']);
+        $this->assertAction($params['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
+
+}
+
+
+
+
+
+
+
