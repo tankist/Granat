cf21e32de39e3699ed6f110fa05305cdca4d5be1
Victor Gryshko <stalin1945@gmail.com>
2011-05-12 16:24:48 +0300

    Added: Categories controller testcase

---
+++ tests/application/modules/admin/controllers/CategoriesControllerTest.php
@@ -0,0 +1,86 @@
+<?php
+
+class Admin_CategoriesControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
+{
+
+    public function setUp()
+    {
+        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        parent::setUp();
+    }
+
+    public function testIndexAction()
+    {
+        $params = array('action' => 'index', 'controller' => 'Categories', 'module' => 'admin');
+        $url = $this->url($this->urlizeOptions($params));
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($params['module']);
+        $this->assertController($params['controller']);
+        $this->assertAction($params['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
+    public function testAddAction()
+    {
+        $params = array('action' => 'add', 'controller' => 'Categories', 'module' => 'admin');
+        $url = $this->url($this->urlizeOptions($params));
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($params['module']);
+        $this->assertController($params['controller']);
+        $this->assertAction($params['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
+    public function testEditAction()
+    {
+        $params = array('action' => 'edit', 'controller' => 'Categories', 'module' => 'admin');
+        $url = $this->url($this->urlizeOptions($params));
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($params['module']);
+        $this->assertController($params['controller']);
+        $this->assertAction($params['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
+    public function testDeleteAction()
+    {
+        $params = array('action' => 'delete', 'controller' => 'Categories', 'module' => 'admin');
+        $url = $this->url($this->urlizeOptions($params));
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($params['module']);
+        $this->assertController($params['controller']);
+        $this->assertAction($params['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
