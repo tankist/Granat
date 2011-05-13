39dcde31481cdc18db1e07a8115ad552157da807
Victor <stalin1945@gmail.com>
2011-05-14 00:21:39 +0300

    granat-247: Добавить возможность выбора другого изображения для просмотра

--- www/js/model.js
+++ www/js/model.js
@@ -4,5 +4,13 @@
 			verticalDragMinHeight:20,
 			verticalDragMaxHeight:20
 		});
+
+	    $('#thumbnails .profile-image a').click(function(e) {
+		    e.preventDefault();
+		    var src = $(this).data('src');
+		    if (src.length > 0) {
+			    $('#image img').attr('src', src);
+		    }
+	    })
     });
 }(this, this.document, this.jQuery));
