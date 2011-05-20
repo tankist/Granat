3bc8605b9a8d3e010fa7e860afdd2f1106d2d92e
Victor <stalin1945@gmail.com>
2011-05-20 16:45:00 +0300

    granat-266: Добавить мультиязычность Fixed: views and layouts Added: translations to the language files

--- data/locales/en_US.php
+++ data/locales/en_US.php
@@ -12,5 +12,20 @@ return array(
 	'Коллекция тканей' => 'Fabrics collection',
 	'Услуги' => 'Service',
 
-	
+	'Как нас найти' => 'How to find us?',
+
+	'Посмотреть на карте' => 'See on map',
+
+	'Наши коллекции' => 'Our collections',
+	'Предыдущая модель' => 'Previous model',
+	'Следующая модель' => 'Next model',
+	'Назад к коллекции' => 'Back to the collection',
+
+	'Страница' => 'Page',
+	'Коллекция' => 'Collection',
+	'Контактная информация' => 'Contact information',
+	'Модели' => 'Models',
+	'Главная' => 'Home',
+	'Ателье ГраНат в Николаеве' => 'GraNat studio - Mykolaiv',
+	'Все права защищены' => 'All rights reserved'
 );
