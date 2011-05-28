748ebeabca6684a7b100a3c89b640c2c7e38ab89
Victor <stalin1945@gmail.com>
2011-05-28 18:16:04 +0300

    granat-278: Добавить кэширование конфига

---
+++ library/Skaya/Application.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once 'Zend/Application.php';
+
+class Skaya_Application extends Zend_Application {
+
+	/**
+	 * @var Zend_Cache_Core
+	 */
+	protected $_configCache;
+
+	/**
+	 * @param string $environment
+	 * @param array|Zend_Config $options
+	 * @param Zend_Cache_Core $cache
+	 */
+	public function __construct($environment, $options = null, Zend_Cache_Core $cache) {
+		$this->setConfigCache($cache);
+		parent::__construct($environment, $options);
+	}
+
+	/**
+	 * @param Zend_Cache_Core $configCache
+	 * @return Skaya_Application
+	 */
+	public function setConfigCache(Zend_Cache_Core $configCache) {
+		$this->_configCache = $configCache;
+		return $this;
+	}
+
+	/**
+	 * @return Zend_Cache_Core
+	 */
+	public function getConfigCache() {
+		return $this->_configCache;
+	}
+
+	/**
+	 * Get cache ID for save/retrieve cached config
+	 * @param  $file
+	 * @return string
+	 */
+	protected function _cacheId($file) {
+		return md5($file . '_' . $this->getEnvironment());
+	}
+
+	protected function _loadConfig($file) {
+		$suffix = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+		if (!($cache = $this->getConfigCache()) || in_array($suffix, array('php', 'inc'))) {
+			return parent::_loadConfig($file);
+		}
+		$cache_id = $this->_cacheId($file);
+		$configLastModified = filemtime($file);
+		$cacheLastModified = $cache->test($cache_id);
+		if ($cacheLastModified !== false && $cacheLastModified > $configLastModified) {
+			return $cache->load($cache_id);
+		}
+		$config = parent::_loadConfig($file);
+		$cache->save($config, $cache_id, array(), null);
+		return $config;
+	}
+
+}
--- www/index.php
+++ www/index.php
@@ -11,16 +11,26 @@ defined('APPLICATION_ENV')
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
+    get_include_path()
 )));
 
 /** Zend_Application */
-require_once 'Zend/Application.php';
+require_once 'Zend/Cache/Core.php';
+require_once 'Zend/Cache/Backend/File.php';
+require_once 'Skaya/Application.php';
+$configCache = new Zend_Cache_Core(array(
+	'automatic_serialization'=>true
+));
+$backend = new Zend_Cache_Backend_File(array(
+	'cache_dir' => realpath(APPLICATION_PATH . '/../cache')
+));
+$configCache->setBackend($backend);
 
 // Create application, bootstrap, and run
-$application = new Zend_Application(
+$application = new Skaya_Application(
     APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
+    APPLICATION_PATH . '/configs/application.ini',
+	$configCache
 );
 $application->bootstrap()
             ->run();
