687e5dd0a84ff3a6ebc61e5bcfdd41261e3103ae
Victor Gryshko <stalin1945@gmail.com>
2011-05-31 18:03:39 +0300

    Added: cache save/delete mapper methods support for all mappers decorators

--- application/models/mappers/Decorator/Cache/Collection.php
+++ application/models/mappers/Decorator/Cache/Collection.php
@@ -2,4 +2,27 @@
 
 class Model_Mapper_Decorator_Cache_Collection extends Skaya_Model_Mapper_Decorator_Cache {
 
+    public function getCacheId($method, $params) {
+        switch ($method) {
+            case 'save':
+            case 'delete':
+                $data = array_shift($params);
+                if (is_array($data) && array_key_exists('id', $data)) {
+                    return 'collection_' . $data['id'];
+                }
+                break;
+        }
+        return parent::getCacheId($method, $params);
+    }
+
+    public function getCacheTags($method, $params) {
+        switch ($method) {
+            case 'save':
+            case 'delete':
+                return array('list', 'collections');
+                break;
+        }
+        return parent::getCacheTags($method, $params);
+    }
+
 }
--- application/models/mappers/Decorator/Cache/User.php
+++ application/models/mappers/Decorator/Cache/User.php
@@ -2,4 +2,27 @@
 
 class Model_Mapper_Decorator_Cache_User extends Skaya_Model_Mapper_Decorator_Cache {
 
+    public function getCacheId($method, $params) {
+        switch ($method) {
+            case 'save':
+            case 'delete':
+                $data = array_shift($params);
+                if (is_array($data) && array_key_exists('id', $data)) {
+                    return 'user_' . $data['id'];
+                }
+                break;
+        }
+        return parent::getCacheId($method, $params);
+    }
+
+    public function getCacheTags($method, $params) {
+        switch ($method) {
+            case 'save':
+            case 'delete':
+                return array('list', 'users');
+                break;
+        }
+        return parent::getCacheTags($method, $params);
+    }
+    
 }
