8e212f0b30c223e7e8c3c7bc6efc9b5948da63e4
tankist <stalin1945@gmail.com>
2011-10-12 02:46:33 +0300

    Added: Page cache

--- www/index.php
+++ www/index.php
@@ -2,35 +2,64 @@
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+|| define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
 // Define application environment
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path()
-)));
+                                           realpath(APPLICATION_PATH . '/../library'),
+                                           get_include_path()
+                                       )));
 
 /** Zend_Application */
+require_once 'Zend/Cache.php';
+
+$frontendOptions = array(
+    'lifetime' => 7200,
+    'regexps' => array(
+        // кэширование всего IndexController
+        '^/.*$' => array(
+            'cache' => true,
+            'cache_with_get_variables' => true,
+            'cache_with_cookie_variables' => true,
+            'make_id_with_cookie_variables' => false
+        ),
+
+        // не кэшируем админку
+        '^/admin/*' => array('cache' => false)
+    )
+);
+
+$backendOptions = array(
+    'cache_dir' => APPLICATION_PATH . '/../cache/page/'
+);
+
+// получение объекта Zend_Cache_Frontend_Page
+/** @var $cache Zend_Cache_Frontend_Page */
+$cache = Zend_Cache::factory('Page',
+                             'File',
+                             $frontendOptions,
+                             $backendOptions);
+
+$cache->start();
+
 require_once 'Zend/Cache/Core.php';
-require_once 'Zend/Cache/Backend/File.php';
 require_once 'Skaya/Application.php';
-$configCache = new Zend_Cache_Core(array(
-	'automatic_serialization'=>true
-));
-$backend = new Zend_Cache_Backend_File(array(
-	'cache_dir' => realpath(APPLICATION_PATH . '/../cache')
-));
-$configCache->setBackend($backend);
+
+$configCache = Zend_Cache::factory(
+    'Core', 'File',
+    array('automatic_serialization' => true),
+    array('cache_dir' => realpath(APPLICATION_PATH . '/../cache'))
+);
 
 // Create application, bootstrap, and run
 $application = new Skaya_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/configs/application.ini',
-	$configCache
+    $configCache
 );
 $application->bootstrap()
-            ->run();
+    ->run();
